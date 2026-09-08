54623a72bc30799d843ba6fb26d68ac4eb3b2af2
winterwind999 <realjordanfaciol@gmail.com>
2026-09-08 16:52:37 +0800

    test 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { APP_VERSION } from './core/version';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-spaversion');
+
+  version = signal(APP_VERSION);
 }
